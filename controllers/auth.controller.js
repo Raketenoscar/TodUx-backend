7614231952521fe1ec7ddead6fbbcacde9919907
Oscar Jeschke <oscarjeschke@outlook.de>
2026-04-03 23:29:46 +0200

    Forgot to remove a session var xD

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -25,7 +25,7 @@ export const signUp = async (req, res, next) => {
       password: hashedPassword,
     });
 
-    const newUsers = await User.create([newUser], { session });
+    const newUsers = await User.create([newUser]);
 
     const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
       expiresIn: JWT_EXPIRES_IN,
